0a9690d7275167580ac8892d7e4ff6008d115bde
george <georgeutm2014@gmail.com>
2025-05-21 00:21:32 -0600

    ajuste prueba

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,24 @@
-FROM node:22.11-alpine3.20
+# Etapa de construcción
+FROM node:22.11-alpine3.20 AS build
 WORKDIR /app
 COPY package*.json ./
 RUN npm install
 COPY . .
-EXPOSE 5173
-CMD [ "npm","run","dev" ]
+RUN npm run build
+
+# Etapa de producción
+FROM nginx:stable-alpine
+COPY --from=build /app/dist /usr/share/nginx/html
+EXPOSE 80
+# Configurar Nginx para manejar rutas de SPA
+RUN echo 'server { \
+    listen 80; \
+    server_name localhost; \
+    location / { \
+        root /usr/share/nginx/html; \
+        index index.html index.htm; \
+        try_files $uri $uri/ /index.html; \
+    } \
+}' > /etc/nginx/conf.d/default.conf
+
+CMD ["nginx", "-g", "daemon off;"]
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-
+import { useState } from "react";
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    if (count === 9) {
+      setCount(0);
+    } else {
+      setCount(count + 1);
+    }
+  }
+
+  const decrement = () => {
+    if (count === 0) {
+      setCount(10);
+    } else {
+      setCount(count - 1);
+    }
+  }
+
   return (
     <>
       <div className="container">
       <h1 className="world">
-        Hello world! 2
+        esto esta perron {count}
       </h1>
+
+      <input type="button" value="increment" onClick={increment} />
+      <input type="button" value="decrement" onClick={decrement} />
       </div>
       
     </>
